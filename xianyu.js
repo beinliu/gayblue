645e921cd4bcf15ebca1f62c9aa22e65bfd70183
beinliu <beinliu@hotmail.com>
2015-12-12 03:31:38 -0700

    Update xianyu.js

--- xianyu.js
+++ xianyu.js
@@ -1,4 +1,4 @@
 setInterval(function() {
  $('[id="tab-event"]').trigger('tap');
- $('[data-quest-id="707551"]'+'[data-bp="1"]').trigger('tap');
+ setTimeout(function() {$('[data-quest-id="707551"]'+'[data-bp="1"]').trigger('tap')},1000;
 }, 5000);
