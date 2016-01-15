1f4d51287f8713634f4f2c62756fab36f4c3ed12
beinliu <beinliu@hotmail.com>
2016-01-14 22:00:57 -0700

    Update singledog.js

--- singledog.js
+++ singledog.js
@@ -2,3 +2,4 @@ setInterval(function() {
  $('[id="tab-event"]').trigger('tap');
 setTimeout(function() {$('[data-quest-id="708151"]'+'[data-bp="1"]').trigger('tap')},1000);
 }, 5000);
+console.info('1BP舔四象中.')
