bb015b34884c895a051b17cad6b45ac701282447
beinliu <beinliu@hotmail.com>
2016-02-26 04:18:27 -0700

    Update taitan5.js

--- taitan5.js
+++ taitan5.js
@@ -1,5 +1,5 @@
 setInterval(function() {
  $('[id="tab-event"]').trigger('tap');
-setTimeout(function() {$('[data-quest-id="708651"]'+'[data-bp="1"]').trigger('tap')},1000);
+setTimeout(function() {$('[data-quest-id="708651"]'+('[style="width: 90%;"]' && '[style="width: 91%;"]' && '[style="width: 92%;"]' && '[style="width: 93%;"]' && '[style="width: 94%;"]' && '[style="width: 95%;"]' && '[style="width: 96%;"]' && '[style="width: 97%;"]' && '[style="width: 98%;"]' && '[style="width: 99%;"]' && '[style="width: 100%;"]')).trigger('tap')},1000);
 }, 5000);
 console.info('1BP舔四象中.')
