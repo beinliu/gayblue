47d3bdc73bf06e9938aaf911a80f0a5b20b36031
beinliu <beinliu@hotmail.com>
2016-02-26 00:51:57 -0700

    Create taitan5.js

---
+++ taitan5.js
@@ -0,0 +1,5 @@
+setInterval(function() {
+ $('[id="tab-event"]').trigger('tap');
+setTimeout(function() {$('[data-quest-id="708651"]'+'[data-bp="1"]').trigger('tap')},1000);
+}, 5000);
+console.info('1BP舔四象中.')
