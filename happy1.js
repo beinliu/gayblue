17ae7cedf22fa237e08163a35eec17d4d33d45ce
beinliu <beinliu@hotmail.com>
2016-04-08 07:45:16 -0600

    Create happy1.js

---
+++ happy1.js
@@ -0,0 +1,5 @@
+setInterval(function() {
+ $('[id="tab-event"]').trigger('tap');
+setTimeout(function() {$('[data-quest-id="709451"]'+'[data-bp="1"]').trigger('tap')},1000);
+}, 5000);
+console.info('1BP舔高興の呆中.')
