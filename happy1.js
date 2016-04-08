8d811c1dbc8dceb278a2e23ced727ce10a321095
beinliu <beinliu@hotmail.com>
2016-04-08 07:54:48 -0600

    Update happy1.js

--- happy1.js
+++ happy1.js
@@ -2,4 +2,4 @@ setInterval(function() {
  $('[id="tab-event"]').trigger('tap');
 setTimeout(function() {$('[data-quest-id="709451"]'+'[data-bp="1"]').trigger('tap')},1000);
 }, 5000);
-console.info('1BP舔高興の呆中.')
+console.info('1BP prpr高興の呆中.')
